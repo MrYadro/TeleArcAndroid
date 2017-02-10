5c34fa28b095d973c20047043d8090a431c712e4
MrYadro <yadrishnikovyaroslav@gmail.com>
2017-02-11 01:35:57 +0700

    remove .image because now we can use solid color

--- main.py
+++ main.py
@@ -8,22 +8,13 @@ def convertFromSignedHex(s):
 
 for filename in filenames:
     src = open(filename + ".atthemesrc", "r")
-    img = open(filename + ".image", "rb")
-
     theme = open(filename + ".attheme", "w")
-
-    image = img.read()
 
     for line in src:
         magicColor = line.strip().split("=")
-
         swapedColor = magicColor[1][-2:]+magicColor[1][:6]
-
         i = convertFromSignedHex(swapedColor)
         theme.write(magicColor[0]+"="+str(i)+"\n")
-
-    theme.close()
-    theme = open(filename + ".attheme", "ab")
-    theme.write(image)
+        
     src.close()
     theme.close()
